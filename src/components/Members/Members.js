0453d913faeed261d786e12237aa7917893b8bfd
haimski <benzak@gmail.com>
2019-05-19 18:27:24 +0300

    adding panels

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,53 +1,73 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function Members() {
-  return (
-      <div class="panel panel-default">
-          <div class="panel-heading">
+class Members extends Component {
+ 
+   state={
+       data:null
+   }
+
+    componentDidMount(){
+        setInterval(async () => {
+          const res = await fetch('http://localhost:3000/teams')
+          const data = await res.json()
+          this.setState({ data })  
+      }, 1000)
+  }
+
+  render(){
+        return (
+          <div class="panel panel-default">
+            <div class="panel-heading">
               <h2>
-                  <i class="fa fa-flag-o red" />
-                  <strong>Members</strong>
+                <i class="fa fa-flag-o red" />
+                <strong>Members</strong>
               </h2>
               <div class="panel-actions">
-                  <a href="index.html#" class="btn-setting">
-                      <i class="fa fa-rotate-right" />
-                  </a>
-                  <a href="index.html#" class="btn-minimize">
-                      <i class="fa fa-chevron-up" />
-                  </a>
-                  <a href="index.html#" class="btn-close">
-                      <i class="fa fa-times" />
-                  </a>
+                <a href="index.html#" class="btn-setting">
+                  <i class="fa fa-rotate-right" />
+                </a>
+                <a href="index.html#" class="btn-minimize">
+                  <i class="fa fa-chevron-up" />
+                </a>
+                <a href="index.html#" class="btn-close">
+                  <i class="fa fa-times" />
+                </a>
               </div>
-          </div>
-          <div class="panel-body">
+            </div>
+            <div class="panel-body">
               <table class="table bootstrap-datatable countries members">
-                  <thead>
-                      <tr>
-                          <th>ID</th>
-                          <th>Name</th>
-                          <th>Role</th>
-                          <th>Task</th>
-                          <th>Location</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td>744914</td>
-                          <td>Michael Lochinov</td>
-                          <td>Developer</td>
-                          <td>US2326</td>
-                          <td>
-                              <img
-                                  src="img/Germany.png"
-                                  style={{ height: "18px", marginTop: "-2px" }}
-                              />{" "}
-                              Israel
-                        </td>
-                      </tr>
-                  </tbody>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Task</th>
+                    <th>Location</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.data?this.state.data.map(team => team.members.map(member => 
+                                <tr>
+                                <td>{member.id}</td>
+                                <td>{member.fullname}</td>
+                                <td>{member.role}</td>
+                                <td>{member.task}</td>
+                                <td>
+                                    <img
+                                    src={`img/${member.location}.png`}
+                                    style={{ height: "18px", marginTop: "-2px" }}
+                                    />{" "}
+                                    {member.location}
+                                </td>
+                                </tr>
+                  )):null}
+                </tbody>
               </table>
+            </div>
           </div>
-      </div>
-  )
+        );
+  }
+  
 }
+
+export default Members;
--- src/components/SonarIssues/SonarIssues.js
+++ src/components/SonarIssues/SonarIssues.js
@@ -1,38 +1,80 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-export default function SonarIssues() {
-  return (
+
+class SonarIssues extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    setInterval(async () => {
+      const res = await fetch("http://localhost:3000/sonar");
+      const data = await res.json();
+    this.setState({ data });
+    }, 1000)
+  }
+
+  render() {
+       
+    let temp = this.state.data?this.state.data.filter(
+         obj =>
+           obj.severity == "blocker" ||
+           obj.severity == "major" ||
+           obj.severity == "critical"
+       ):null
+    
+       let temp2 =this.state.data?this.state.data.filter(
+         obj =>
+           obj.severity != "blocker" &&
+           obj.severity != "major" &&
+           obj.severity != "critical"
+       ):null
+     
+       let sorted =temp? temp.concat(temp2):null;
+
+    return (
       <section class="panel">
-          <div class="panel-body progress-panel sonar">
-              <div class="row">
-                  <div class="col-lg-8 task-progress pull-left">
-                      <h1>Sonar Issues</h1>
-                  </div>
-                  <div class="col-lg-4">
-                      <span class="profile-ava pull-right" />
-                  </div>
-              </div>
+        <div class="panel-body progress-panel sonar">
+          <div class="row">
+            <div class="col-lg-8 task-progress pull-left">
+              <h1>Sonar Issues</h1>
+            </div>
+            <div class="col-lg-4">
+              <span class="profile-ava pull-right" />
+            </div>
           </div>
-          <table class="table table-hover personal-task">
-              <thead>
-                  <th>id</th>
-                  <th>Status</th>
-                  <th>Severity</th>
-                  <th>Asignee</th>
-              </thead>
-              <tbody>
+        </div>
+        <table class="table table-hover personal-task">
+          <thead>
+            <th>id</th>
+            <th>Status</th>
+            <th>Severity</th>
+            <th>Asignee</th>
+          </thead>
+          <tbody>
+            {sorted
+              ? sorted.map(sonar => (
                   <tr>
-                      <td>12</td>
-                      <td>Open</td>
-                      <td>
-                          <label class="label label-danger">Blocker</label>
-                      </td>
-                      <td>
-                          <div class="asignee">Snir Dahan</div>
-                      </td>
+                    <td>{sonar.id}</td>
+                    <td>{sonar.status}</td>
+                    <td>
+                      {
+                        sonar.severity == 'blocker' || sonar.severity == 'major' || sonar.severity == 'critical'?
+                        <label class="label label-danger">{sonar.severity}</label>
+                        :<label class="label" style={{color:'black'}}>{sonar.severity}</label>
+                      }
+                    </td>
+                    <td>
+                      <div class="asignee">{sonar.asignee}</div>
+                    </td>
                   </tr>
-              </tbody>
-          </table>
+                ))
+              : null}
+          </tbody>
+        </table>
       </section>
-  )
+    );
+  }
 }
+
+export default SonarIssues;
